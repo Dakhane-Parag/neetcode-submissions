0cb60b55b445bb82e985601a8859042148bb7b4b
Dakhane Parag <162367195+Dakhane-Parag@users.noreply.github.com>
2026-07-31 12:34:22 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,30 @@
+class Solution:
+    "isalnum() this is an inbuilt function to check if the character is a alphanumeric character"
+    def isAlphaNumeric(self,c):
+        return (
+            ord('A') <= ord(c) <= ord('Z') or 
+            ord('a') <= ord(c) <= ord('z') or 
+            ord('0') <= ord(c) <= ord('9')
+        )
+
+    def isPalindrome(self, s: str) -> bool:
+        i = 0
+        j = len(s) -1
+
+        while(i<j):
+            while i< j and not self.isAlphaNumeric(s[i]):
+                i+=1
+
+            while i < j and not self.isAlphaNumeric(s[j]):
+                j-=1
+            
+            if s[i].lower() != s[j].lower():
+                return False
+            
+            i += 1
+            j -= 1
+
+        return True
+
+
+        
